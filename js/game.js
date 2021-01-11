90d0091767648d5b858300e9637a7c46aaadde98
Gryfs <Allan18040@gmail.com>
2021-01-11 10:34:07 +0100

    suite

--- js/game.js
+++ js/game.js
@@ -1,1 +1,15 @@
-console.log("Hello :")
+const canvas = document.getElementById('canvasTest');
+const ctx = canvas.getContext('2d')
+
+let x = 10
+
+function gameLoop() {
+    ctx.fillStyle = 'yellow';
+    ctx.fillRect(0,0,300,300);
+
+    ctx.fillStyle = 'red';
+    ctx.fillRect(x,10,100,100);
+    x += 2
+}
+
+setInterval(gameLoop, 1000 / 60)
