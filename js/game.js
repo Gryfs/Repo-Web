092ca1dd9d9a0695631a4efc9c8eff2d7448d6f4
Gryfs <Allan18040@gmail.com>
2021-01-11 11:24:15 +0100

    bn,v

--- js/game.js
+++ js/game.js
@@ -1,15 +1,45 @@
-const canvas = document.getElementById('canvasTest');
-const ctx = canvas.getContext('2d')
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
 
-let x = 10
+let x = 20
+let y = 20
 
-function gameLoop() {
-    ctx.fillStyle = 'white';
-    ctx.fillRect(0,0,300,300);
+let rectWidth = 100
+let rectHeight = 100
 
-    ctx.fillStyle = 'red';
-    ctx.fillRect(x,10,100,100);
-    x += 2
+let goUp = new Boolean ("false");
+let goRight = new Boolean("true")
+
+
+function gameloop(){
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    
+    ctx.fillStyle = 'green';
+    ctx.fillRect(x, y, rectWidth, rectHeight);
+    
+    if (goUp) {
+        y += 1
+    }
+    else{
+        y -= 1
+    }
+
+    if (goRight) {
+        x += 1
+    }
+    else{
+        x -= 1
+    }
+
+    if (x+rectWidth >= canvas.width || x <= 0) {
+        goRight = !goRight
+    }
+
+    if (y+rectHeight >= canvas.height || y <= 0) {
+        goUp = !goUp
+    }
+
 }
 
-setInterval(gameLoop, 1000 / 60)
+setInterval(gameloop, 1000 / 60)
