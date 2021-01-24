f4d5e4a22562c9b6b1e68de9d9d3613e5c49089e
Gryfs <Allan18040@gmail.com>
2021-01-24 17:21:21 +0100

    jhgf
    fjhd

--- js/game.js
+++ js/game.js
@@ -1,15 +1,33 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
+canvas.width = document.documentElement.clientWidth || document.body.clientWidth;
+canvas.height = document.documentElement.clientHeight || document.body.clientHeight;
+
 let x = 20
 let y = 20
 
 let rectWidth = 100
 let rectHeight = 100
 
-let goUp = new Boolean ("false");
-let goRight = new Boolean("true")
+let ColorCircleBorder = 'white';
+let borderThickness = 8;
 
+let goUp = new Boolean ("false");
+let goRight = new Boolean("true");
+
+var colors = "#FF0000";
+
+const gameObjects = [];
+
+
+function startup() {
+    var el = document.getElementById("canvas");
+    el.addEventListener("touchstart", handleStart, false);
+    el.addEventListener("touchend", handleEnd, false);
+  }
+  
+  document.addEventListener("DOMContentLoaded", startup);
 
 function gameloop(){
     ctx.fillStyle = 'black';
@@ -39,7 +57,45 @@ function gameloop(){
     if (y+rectHeight >= canvas.height || y <= 0) {
         goUp = !goUp
     }
+   
+    
 
 }
 
-setInterval(gameloop, 1000 / 60)
+function handleStart(ev) {
+    ev.preventDefault();
+
+    var touches = ev.changedTouches;
+
+    for (var i = 0; i < touches.length; i++) {
+        console.log("Touche " + i);
+        const x = ev.touches[i].clientX;
+        const y = ev.touches[i].clientY;
+        const r = Math.random() * 100 + 50;
+        const color = colors[Math.floor(Math.random() * colors.length)];
+        // Ajoute un nouveau cerlcle au tableau gameobjects
+        gameObjects.push(cercle_create(x, y, r, color, 3, 8, Math.random() >= 0.5));
+    }
+}
+
+function cercle_create(x, y, rayon, color, angleStart, angleEnd, sensAntiHoraire) {
+    console.log("Create new circle");
+    
+    ctx.beginPath();
+    ctx.fillStyle = color;
+    ctx.strokeStyle = ColorCircleBorder
+    ctx.lineWidth = borderThickness;
+    ctx.arc(Math.random() * canvas.width, Math.random() * canvas.height, 50, 0, Math.PI * 2); 
+    ctx.fill();
+    ctx.stroke();
+}
+
+
+
+function handleEnd(ev) {
+    ev.preventDefault();
+    
+}
+
+
+setInterval(gameloop, 1000 / 10)
